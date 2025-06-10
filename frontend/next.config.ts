6e736c99d15510059b8a5caa5f3e05132d47fdb9
usmasa <usman@softwarealliance.dk>
2025-06-10 18:23:57 +0500

    vercel.json

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+/* import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
 };
 
 export default nextConfig;
+ */
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true, // if using app directory
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/:path*", // Proxy to Backend
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
